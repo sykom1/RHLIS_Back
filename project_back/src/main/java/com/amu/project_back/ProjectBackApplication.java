d47678619094fedee67ecf8ce2cca59ab3cabea9
sykom1 <saber.tnifas@etu.univ-amu.fr>
2022-02-01 14:57:00 +0100

    initialisation du projet

---
+++ project_back/src/main/java/com/amu/project_back/ProjectBackApplication.java
@@ -0,0 +1,13 @@
+package com.amu.project_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBackApplication.class, args);
+    }
+
+}
